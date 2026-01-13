2ba6c14812f24e90d111c932fd14c46ddc506469
mohamedzljm <mohamed.zljm@gmail.com>
2026-01-13 16:45:45 +0100

    Eerste versie van de classe BagagePersoneel.

---
+++ src/BagagePersoneel.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+    public class BagagePersoneel extends Personeelslid{
+        public BagagePersoneel(String voornaam, String achternaam, String rijksregisternummer, LocalDate geboortedatum){
+            super(voornaam, achternaam, rijksregisternummer, geboortedatum);}
+
+        @Override
+        public String toString() {
+            return "BagagePersoneel{}";
+        }
+    }
+
